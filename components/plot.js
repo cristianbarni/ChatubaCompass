72e0ce9057006a21233718431947abfed4a8c4e4
Barni <cristian.m.barni@gmail.com>
2021-02-06 00:44:29 -0300

    Improved layout to appear on mobile
    It's still crappy, but at least it works

--- components/plot.js
+++ components/plot.js
@@ -3,6 +3,7 @@ import { Bubble } from 'react-chartjs-2';
 import styles from '../components/layout.module.css'
 
 var options = {
+  maintainAspectRatio: true,
   scales: {
     yAxes: [{
       display: true,
@@ -33,33 +34,43 @@ var options = {
   }
 };
 
-var data = {
+let data = {
   datasets: []
 };
 
+let data_id = [];
+
 export default function Plot({ dbData }) {
 
-  {
-    dbData.map((dados) => (
+  for (let dados of dbData) {
+    if (!data_id.includes(dados._id)) {
+      data_id.push(dados._id)
       data.datasets.push(
         {
           label: dados.Name,
           backgroundColor: dados.backgroundColor,
           borderColor: dados.borderColor,
-          // backgroundColor: pickColor('backgroundColor'),
-          // borderColor: pickColor('borderColor'),
           data: [{ x: dados.SG, y: dados.LP, r: dados.Votes }]
         }
       )
-    ))
+    } else {
+      console.log(data.datasets)
+      console.log(dados.Name)
+      console.log(data.datasets.includes(dados.Name))
+    }
   }
 
   return (
-    <div className={styles.plot}>
-      <Bubble
-        data={data}
-        options={options}
-      />
+    <div className={styles.header}>
+      <div className={styles.plot}>
+        <Bubble
+          data={data}
+          options={options}
+          width={700}
+          height={450}
+        />
+      </div>
     </div>
+    
   );
 }
